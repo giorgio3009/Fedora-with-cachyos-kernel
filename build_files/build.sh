1297464062c33fa092d363db89c14d31cdc8f239
giorgio3009 <fastamegiorgio@gmail.com>
2026-06-16 21:02:28 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -21,18 +21,22 @@ enabled_metadata=1
 EOF
 
 # Installazione RPMFusion
-dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-               https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+                https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 ### 3. Install Packages
-# Kernel CachyOS (usiamo --setopt=tsflags=noscripts per evitare errori di dracut durante la build)
-dnf install -y kernel-cachyos --setopt=tsflags=noscripts
+# Pulizia del kernel di default per evitare errori del linter bootc
+dnf5 remove -y kernel kernel-core kernel-modules --allowerasing
 
-# Installazione Ambiente Desktop
+# Installazione Kernel CachyOS
+dnf5 install -y kernel-cachyos --setopt=tsflags=noscripts
+
+# Aggiornamento metadati e Installazione Ambiente Desktop
+dnf5 makecache
 dnf5 group install -y workstation-product-environment
 
-# Driver NVIDIA (akmod compilerà i moduli al primo boot)
-dnf install -y akmod-nvidia
+# Driver NVIDIA
+dnf5 install -y akmod-nvidia
 
 ### 4. Enable Services
 systemctl enable gdm.service
@@ -42,6 +46,6 @@ systemctl enable nvidia-hibernate.service
 systemctl enable nvidia-resume.service
 
 ### 5. Cleanup
-# Pulizia per ridurre le dimensioni dell'immagine
-dnf clean all
+# Pulizia finale per ridurre le dimensioni dell'immagine
+dnf5 clean all
 rm -rf /run/dnf /run/selinux-policy /var/lib/dnf
